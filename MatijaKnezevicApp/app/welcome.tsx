d8c9a4134a8028facc3612116ce0643fb9865078
MatijaKnezevic <95369523+MatijaSKnezevic@users.noreply.github.com>
2025-12-05 13:50:37 +0100

    Add files via upload

--- MatijaKnezevicApp/app/welcome.tsx
+++ MatijaKnezevicApp/app/welcome.tsx
@@ -1,41 +1,50 @@
-import {Button, Text, StyleSheet, TouchableOpacity, ActivityIndicator} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {useAuth} from "@/context/AuthContext";
-import {Redirect, useRouter} from "expo-router";
-import {useEffect, useState} from "react";
+import { Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
+const APP_VERSION = "1.0.0";
 
 const WelcomeScreen = () => {
-
-    const {session} = useAuth();
+    const { session } = useAuth();
     const router = useRouter();
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const checkFirstOpen = async () => {
+
+
+    useEffect(() =>  {
+        const init = async () => {
+
+            const savedVersion = await AsyncStorage.getItem("appVersion");
+
+            if (savedVersion !== APP_VERSION) {
+
+                await AsyncStorage.clear();
+                await AsyncStorage.setItem("appVersion", APP_VERSION);
+            }
 
             const alreadyOpened = await AsyncStorage.getItem("welcomeScreenSeen");
 
-            if(alreadyOpened){
-                if(session) return router.replace("/(tabs)");
-                return router.replace("/login");
-            }
+            if (alreadyOpened === "true") {
+                    router.replace("/login");
+                }
+
             setLoading(false);
-
         }
-        checkFirstOpen();
+            init();
     }, [session]);
-
 
     const handleButtonStart = async () => {
         await AsyncStorage.setItem("welcomeScreenSeen", "true");
-
         router.replace("/login");
+    };
+
+    if (loading) {
+        return <ActivityIndicator size="large" style={{ marginTop: 50 }} />;
     }
 
-    if (loading) return <ActivityIndicator size="large" style={{marginTop: 50}} />;
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Welcome 👋</Text>
@@ -47,14 +56,14 @@ const WelcomeScreen = () => {
             </TouchableOpacity>
         </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
-    container: {flex:1, justifyContent:"center", alignItems:"center", padding:30, backgroundColor:"#fff"},
-    title: {fontSize:32, fontWeight:"900", marginBottom:10},
-    subtitle: {fontSize:16, color:"#6b6b6b", textAlign:"center", marginBottom:6},
-    button: {marginTop:25, backgroundColor:"#2563eb", paddingVertical:12, paddingHorizontal:30, borderRadius:10},
-    buttonText: {color:"#fff", fontSize:18, fontWeight:"600"},
+    container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 30, backgroundColor: "#fff" },
+    title: { fontSize: 32, fontWeight: "900", marginBottom: 10 },
+    subtitle: { fontSize: 16, color: "#6b6b6b", textAlign: "center", marginBottom: 6 },
+    button: { marginTop: 25, backgroundColor: "#2563eb", paddingVertical: 12, paddingHorizontal: 30, borderRadius: 10 },
+    buttonText: { color: "#fff", fontSize: 18, fontWeight: "600" }
 });
 
 export default WelcomeScreen;
